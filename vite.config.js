9669867e1e4f542733a957c4e6ffee15cf49bce1
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2023-08-05 13:42:17 +0530

    Deploying using gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
+export default defineConfig({
     plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/3d-portfolio/'
-  }
-
-  return config
-})
+    base: '/3d-portfolio/',
+});
