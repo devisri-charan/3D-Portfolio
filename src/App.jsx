64fa6ee134a2a7c9338c7ed50eedc12c164e1c62
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2023-08-05 23:08:49 +0530

    Deploying using gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 const App = () => {
 
   return (
-    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/3d-portfolio/'}>
+    <BrowserRouter>
       <div className="relative z-0 bg-primary">
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
               <Navbar />
--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
+export default defineConfig({
     plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/3d-portfolio/'
-  }
-
-  return config
-})
+});
