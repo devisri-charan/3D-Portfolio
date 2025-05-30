fb8667606ec51a1d3e85cfb0dd01b50737e74af7
Charan <vdevisricharan@gmail.com>
2025-05-30 14:32:03 +0530

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
