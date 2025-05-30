da4e1129c1907b9acc14db80fca0f347a778c046
Charan <vdevisricharan@gmail.com>
2025-05-30 13:04:25 +0530

    Shre Charan 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Resume } from "./components";
-import Education from "./components/Education";
+import React from 'react'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={
-            <>
-              <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-                <Hero />
-              </div>
-              <About />
-              <Experience />
-              <Education/>
-              <Tech />
-              <Works />
-              {/* <Feedbacks /> */}
-              <div className="relative z-0">
-                <Contact />
-                <StarsCanvas />
-              </div>
-            </>
-          } />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div>App</div>
   )
 }
 
