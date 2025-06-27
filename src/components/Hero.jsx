a5483f629262963f18fe4cd8256cc42920bbec31
Charan <vdevisricharan@gmail.com>
2025-06-27 18:34:35 +0530

    Update on June 27th 2025

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import { styles } from '../styles';
-// import {ComputersCanvas} from "./canvas";
 
 const Hero = () => {
   return (
@@ -19,7 +18,6 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      {/* <ComputersCanvas /> */}
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
